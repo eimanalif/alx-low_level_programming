8c9ceabf090e5e44ea489b39363656fba302ac48
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-10 15:24:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar,
+	char quo[] = "and that piece of art is useful\" - Dora Korpar,
 
 	     2015-10-19\n";
 
-	write(2, message, 59);
+	write(2, quo, 59);
 
 	return (1);
 }
