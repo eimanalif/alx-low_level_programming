880cdd3fbb7c5da0edd72fc7a3bb59dd8b04429a
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-10 15:27:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
 #include <unistd.h>
 
 /**
- * main - entry point
- * Description:write function
- * Return: always 1 (success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar,
-
-	     2015-10-19\n";
-
-	write(2, quo, 59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
