5369490d8ae75a6bfd84a974ac9363ae2515b484
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-10 12:54:26 -0700

    5- printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * discription:C programme using print
+ * return: always 0 (success)
+ */
+int main(void)
+{	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
