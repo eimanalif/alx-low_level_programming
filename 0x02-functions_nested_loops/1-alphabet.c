d377715eb7c542a428618990d6634b40f752449d
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-15 11:16:22 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+* print alphapet
+*
+*Return:always 0(success)
+*/
+
+void print_alphabet(void);
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
