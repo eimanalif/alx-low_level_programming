9c68403456171a5a18ab5118972808fdf506ca3f
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-23 15:15:29 -0700

    2-stencpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,12 +16,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] !='\0'; i++)
 	{
-		while (i == n)
 
-		dest[n] = src[n];
+		dest[i] = src[i];
 	}
+	while (i < n)
+	{	dest[i] = ('\0');
+		i++;
+	}
+
 	
 	return (dest);
 }
