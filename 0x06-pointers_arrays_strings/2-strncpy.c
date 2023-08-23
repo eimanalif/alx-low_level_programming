d7ec4db23bee46935434e502eaaae02da1ef4d93
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-23 15:20:47 -0700

    2-stencpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,20 +3,17 @@
 /**
  *_strncpy - function that copy
  *
- *Description: function to copy string to another
+ *Description: function to copy string to another 
  *
- *@dest: first string
- *@src: second string
- *@n: number of letter to copy
- *
- *Return: dest
+ *@dest,@src arrays
+ *@n, number of byts to copy
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] !='\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 
 		dest[i] = src[i];
@@ -26,6 +23,5 @@ char *_strncpy(char *dest, char *src, int n)
 		i++;
 	}
 
-	
 	return (dest);
-}
+}	 
