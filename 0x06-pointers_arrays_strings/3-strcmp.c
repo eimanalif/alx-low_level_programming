dfbdd741fe2d6709dd3a21d678d9328206ee81ee
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-24 00:37:01 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ *_strcmp -  function that compares two strings
+ *
+ *Description: function that compares char *s1, char *s2
+ *@s1: string 1
+ *@s2: string to compare
+ *
+ *Return: true 1 , false 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1)
+	{
+	if (*s1 != *s2)
+	{
+		i = (((int)*s1 - 48) - ((int)*s2 - 48));
+		break;
+	}
+		s1++;
+		s2++;
+	}
+	return (i);
+}
