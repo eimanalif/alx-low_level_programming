13217581cfd576b5120687ea31274e55658908d4
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-24 04:55:25 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include"main.h"
+
+/**
+ *cap_string - function that capitalizes all words of a string
+ *Description:function that capitalizes all words of a string
+ *@str:string
+ *Return:str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 97 && str[i] <= 122))
+			i++;
+	if (
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == '}' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == ' ' ||
+		i  == 0)
+		{
+		str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
