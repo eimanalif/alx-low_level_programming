091f15a124f3a55203258d4d229e2e29314cb592
eimanalif <eiman.fadlelmowla@gmail.com>
2023-08-30 12:41:07 -0700

    6-is_prime_number.

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,17 +12,13 @@
 
 int is_prime_number(int n)
 {
-	if ((n % 2 == 0) || (n % 3 == 0))
+	if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
 	{
 		return (0);
 	}
-	if (n <= 0)
+	if (n <= 1)
 	{
 		return (0);
-	}
-	if (n == 1)
-	{
-		return (1);
 	}
 	else
 	{
