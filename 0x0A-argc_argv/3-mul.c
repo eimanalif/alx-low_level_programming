efd610546fd4b9150b42483e06d8611d8e0759d8
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-01 15:51:14 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,17 +14,16 @@ int main(int argc, char *argv[])
 {
 	int sum = 0;
 
-	if (argc != 3)
-	{
-		printf("erorr");
-		printf("\n");
-		return (1);
-	}
 	if (argc == 3)
 	{
 		sum += atoi(argv[1]) * atoi(argv[2]);
-	}
-	printf("%d\n", sum);
 
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("error");
+		return (1);
+	}
 	return (0);
 }
