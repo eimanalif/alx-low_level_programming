974047be34e6f28441f0efe4990980abf2d4f5b4
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-01 19:06:57 -0700

    lib

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ *main -check the code
+ *Description:check lib
+ *Return: 0
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to
+		be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
