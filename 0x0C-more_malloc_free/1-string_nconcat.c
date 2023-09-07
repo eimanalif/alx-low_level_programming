527d9de95acb01b5af77b67da62854388a00ca7b
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-07 13:46:49 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,55 +1,54 @@
 #include"main.h"
 #include<stdlib.h>
-/**
-*_strlen - find length of a string
-*@s:string
-*Return:size
-*
-*/
- 
-int _strlen(char *s)
-{
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
+
+
+
+
+
 /**
 **string_nconcat - a function that concatenates two strings.
 *Description:a function that concatenates two strings.
 *@s1: string
 *@s2:string to concatenates
-*
-*Return: pointer or NULL
-*
- */ 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
- {
-char *s3;
-unsigned int len1, len2;
-if (s1 == NULL)
-{
-s1 = "\0";
-}
-if (s2 == NULL)
-{
-s2 = "\0";
-}
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-s3 = malloc(n * sizeof(char) + 1);
-if (s3 == 0)
-{
-return (0);
-}
-for (n = 0; n < len1 + len2 ; n++)
-{
-if (n < len1)
-s3[n] = s1[n];
-else
-s3[n] = *s1 + s2[n];
-}
-s3[n] = '\0';
-return (s3);
-}
+*@n:number of bytes
+*Return: poiner
+*/
 
+
+
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
+}
