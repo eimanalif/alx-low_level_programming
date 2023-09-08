9965a150715bf886227ab5973ff267145fb4ac69
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-08 03:04:25 -0700

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,18 +22,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
-		p = malloc(new_size);
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
 		return (NULL);
+		free(ptr);
 	}
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+	}
+	return (p);
+
 	if (new_size > old_size)
 	{
 		p = malloc(new_size);
 		for (i = 0; i < old_size && i < new_size; i++)
-		*((char *)ptr + i) = *((char *)p + i);
+		*((char *)p + i) = *((char *)ptr + i);
 		free(ptr);
 	}
 	return (p);
