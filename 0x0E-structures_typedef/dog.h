c78871788d51853bf068562792e496b6df4fc438
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-11 10:37:34 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - id of a dog
+ *Description: provide name , age., owner of a dog
+ *@name:array of string
+ *@age:float
+ *@owner:array of string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
