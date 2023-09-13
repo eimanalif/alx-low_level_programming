6e587d82fb8dbf0513561be1fa0a0fa3611b6659
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-13 14:23:35 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,8 +15,8 @@ int main(int argc, char **argv)
 {
 	int (*op_func)(int, int), a, b;
 
-	if (argc !=4)
-		printf("Error\n"),exit(98);
+	if (argc != 4)
+		printf("Error\n"), exit(98);
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
