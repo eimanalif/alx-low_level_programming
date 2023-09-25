87957e4229ca22d1869af8d5369637a9fa2d93d9
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-25 11:40:56 -0700

    5-fre

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,4 +13,18 @@
 
 void free_listint2(listint_t **head)
 {
-	
+	listint_t *node, *node2;
+
+	if (!head)
+	{
+		return;
+	}	
+	node = *head;
+	while (node)
+	{
+		node2 = node;
+		node =node->next;
+		free(node2);
+	}
+	*head = NULL;
+}
