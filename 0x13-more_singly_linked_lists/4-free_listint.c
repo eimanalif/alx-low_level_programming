a0a3df314d4fe40af810d8fc67935d264cd6e6cc
eimanalif <eiman.fadlelmowla@gmail.com>
2023-09-25 11:25:53 -0700

    4-free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -17,12 +17,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *node;
 
-	if (!head)
-		return;
 
 	while (head)
-	node = head;
-	node = node->next;
-	free(node);
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
 
 }
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include"lists.h"
+
+
+
+
+/**
+ *free_listint2 - a function that frees a listint_t list.
+ *Description:a function that frees a listint_t list.
+ *@head:firstnode pointer
+ *Return:void
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	
