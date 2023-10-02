d60ece7bf7e0661a9cf4ffa57fb8f272d7a37acc
eimanalif <eiman.fadlelmowla@gmail.com>
2023-10-02 14:23:18 -0700

    003

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,35 +22,36 @@
 
 int main(int ac, char **av)
 {
-	int frm_fd = 0, to_fd = 0;
+	int from_fd = 0, to_fd = 0;
 	ssize_t b;
 	char buf[READ_BUFFER_SIZE];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	frm_fd = open(av[1], O_RDONLY);
+	from_fd = open(av[1], O_RDONLY);
 
-	if (frm_fd == -1)
+	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((b = read(frm_fd, buf, READ_BUFFER_SIZE)) > 0)
+	while ((b = read(from_fd, buf, READ_BUFFER_SIZE)) > 0)
 		if (write(to_fd, buf, b) != b)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	frm_fd = close(frm_fd);
+	from_fd = close(from_fd);
 	to_fd = close(to_fd);
 
 
-	if (frm_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, frm_fd), exit(100);
+	if (from_fd)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, frm_fd), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+
 
 	return (EXIT_SUCCESS);
 }
