889e777f5c90796866149c5eaf2306e12af8c290
eimanalif <eiman.fadlelmowla@gmail.com>
2023-11-09 11:38:32 -0800

    a

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include"lists.h"
+
+
+
+/**
+ *dlistint_len - return length
+ *Description:a function that return length 
+ *@h: head pointer
+ *Return: size of list
+ */
+
+
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h) 
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
