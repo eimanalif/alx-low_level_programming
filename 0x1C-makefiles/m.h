10cbf223341811e3a9a9f3b6d081f93ca6eca871
eimanalif <eiman.fadlelmowla@gmail.com>
2024-01-24 09:48:30 -0800

    0-make_file

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,14 @@
+#ifndef __M_H__
+#define __M_H__
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+void print_school(void);
+
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,15 @@
+#include "m.h"
+
+
+
+/**
+ *main - function to print a file
+ *Description:to print a file 
+ *Return:0
+ */
+
+int main(void)
+{
+	print_school();
+	return (0);
+}
